8ef6a847a6eed6f2f10ede5a035a7a1e04487140
Robert Seeger <RHSeeger@gmail.com>
2015-06-09 00:25:37 -0400

    java/problem-1 working

--- Java/problem-1/src/main/java/org/rhseeger/euler/App.java
+++ Java/problem-1/src/main/java/org/rhseeger/euler/App.java
@@ -1,11 +1,15 @@
 package org.rhseeger.euler;
 
+import java.util.stream.IntStream;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println(IntStream.range(1, 1000)
+                .filter(x -> x % 3 == 0 || x % 5 == 0)
+                .sum());
     }
 }
